c5729f1eae7dc49713777ba3d12c3767842c1036
lei <1142908626@qq.com>
2021-06-24 20:41:39 +0800

    feat[eslint]: husky

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,26 @@
 module.exports = {
-    parser: '@typescript-eslint/parser',
-    plugins: ['@typescript-eslint', 'simple-import-sort'],
-    rules: {
-        'no-var': "error",
-        '@typescript-eslint/consistent-type-definitions': [
-            "error",
-            "interface"
-        ],
-        'object-curly-spacing': ["error", "always"],
-        "simple-import-sort/imports": "error",
-        "simple-import-sort/exports": "error",
-        'no-unused-vars': "error"
-    },
-    env:{
-        browser: true,
-        node: true,
-    }
-}
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'simple-import-sort', 'prettier'],
+  rules: {
+    'no-var': 'error',
+    '@typescript-eslint/consistent-type-definitions': ['error', 'interface'],
+    'object-curly-spacing': ['error', 'always'],
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+    'no-unused-vars': 'error',
+    'prettier/prettier': [
+      'error',
+      {
+        semi: true,
+        trailingComma: 'all',
+        singleQuote: true,
+        printWidth: 120,
+        tabWidth: 2,
+      },
+    ],
+  },
+  env: {
+    browser: true,
+    node: true,
+  },
+};
