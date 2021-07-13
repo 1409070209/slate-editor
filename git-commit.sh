ce6684220affdc0f5cbdf6a9e890a59b96319b9d
lei <1142908626@qq.com>
2021-07-13 11:39:28 +0800

    feat: init

--- git-commit.sh
+++ git-commit.sh
@@ -1,22 +1,23 @@
-git filter-branch -f --env-filter '
+#!/bin/sh
+
+git filter-branch --env-filter '
+
 OLD_EMAIL="leinuo14212@ipalfish.com"
 CORRECT_NAME="lei"
 CORRECT_EMAIL="1142908626@qq.com"
+
 if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
 then
     export GIT_COMMITTER_NAME="$CORRECT_NAME"
     export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
 fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
 ' --tag-name-filter cat -- --branches --tags
-git filter-branch --commit-filter '
-        if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ];
-        then
-                GIT_AUTHOR_NAME="$CORRECT_NAME";
-                GIT_AUTHOR_EMAIL="$CORRECT_EMAIL";
-                git commit-tree "$@";
-        else
-                git commit-tree "$@";
-        fi' HEAD
-git config --global user.email leinuo14212@ipalfish.com
+
 git config --global user.name leinuo14212
+git config --global user.email leinuo14212@ipalfish.com
 
